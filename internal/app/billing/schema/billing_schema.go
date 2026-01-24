e85684455b3b73af916c41e3755d39608f12fe0c
bayu <agungbayu0132@mail.com>
2026-01-24 16:18:42 +0700

    update[agung]: finis billing repository

--- internal/app/billing/schema/billing_schema.go
+++ internal/app/billing/schema/billing_schema.go
@@ -1,15 +1,32 @@
 package schema
 
 type Invoice struct {
-	Id int `json:"id"`
+	Id            int     `json:"id"`
+	InvoiceNumber string  `json:"invoice_number"`
+	CustomerId    int     `json:"customer_id"`
+	IssueDate     string  `json:"issue_date"`
+	DueDate       string  `json:"due_date"`
+	Subtotal      float64 `json:"subtotal"`
+	TaxtTotal     float64 `json:"taxt_total"`
+	DiscountTotal float64 `json:"discount_total"`
+	GrandTotal    float64 `json:"grand_total"`
+	Satus         string  `json:"status"`
+	CreatedAt     string  `json:"created_at"`
 }
 
 type InvoiceDetail struct {
-	Id int `json:"id"`
+	Id                   int     `json:"id"`
+	InvoiceId            int     `json:"product_id"`
+	ProductNameSanapshot string  `json:"product_name_snapshot"`
+	Quantity             int     `json:"quantity"`
+	UnitPrice            float64 `json:"unit_price"`
+	TotalPrice           float64 `json:"total_price"`
 }
 
 type CreateInvoiceRequest struct {
-	UserId int     `json:"user_id"`
-	Method string  `json:"method"`
-	Amount float64 `json:"amount"`
+	CustomerId    int     `json:"customer_id"`
+	Subtotal      float64 `json:"subtotal"`
+	TaxtTotal     float64 `json:"taxt_total"`
+	DiscountTotal float64 `json:"discount_total"`
+	GrandTotal    float64 `json:"grand_total"`
 }
